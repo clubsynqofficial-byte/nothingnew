62812411a90e99cb3d53e29571347e2b1c6c9453
abhxy <aby.nair08@gmail.com>
2026-05-20 17:32:12 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import PositionsPage from './pages/positions/PositionsPage'
 import MessagesPage from './pages/messages/MessagesPage'
 import EventsPage from './pages/events/EventsPage'
 import SettingsPage from './pages/settings/SettingsPage'
+import NucleusPortal from './pages/nucleus/NucleusPortal'
 
 function RootRoute() {
   const { session, loading } = useAuth()
@@ -150,6 +151,7 @@ function AppRoutes() {
       <Route path="/messages" element={<ProtectedRoute><MessagesPage /></ProtectedRoute>} />
       <Route path="/events" element={<ProtectedRoute><EventsPage /></ProtectedRoute>} />
       <Route path="/settings" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
+      <Route path="/nucleus" element={<NucleusPortal />} />
 <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   )
